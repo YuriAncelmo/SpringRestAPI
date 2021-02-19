d8c133abba491599b765cde36ee53ffc0b58e925
YuriAncelmo <yuriancelmo@hotmail.com>
2021-02-19 09:09:20 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/webapplication/DemoAplicacaoWeb/DemoAplicacaoWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.webapplication.DemoAplicacaoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAplicacaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAplicacaoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/webapplication/DemoAplicacaoWeb/DemoAplicacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.webapplication.DemoAplicacaoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAplicacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
